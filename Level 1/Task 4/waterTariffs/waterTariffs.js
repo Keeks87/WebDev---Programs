3c1eb5de274528be1dab58d7721fedb88c7885fd
Keenan <110526824+Keeks87@users.noreply.github.com>
2024-08-28 14:14:39 +0100

    Final program

--- Level 1/Task 4/waterTariffs/waterTariffs.js
+++ Level 1/Task 4/waterTariffs/waterTariffs.js
@@ -13,3 +13,35 @@ they have used, and it should output the total a mount in Rands (R) that
 they need to pay.
 */
 
+// Ask the user for the number of litres of water they used
+let userWaterUse = prompt('Please input the number of litres of water used: ')
+
+// Condition for under 6kl water usage
+if (userWaterUse <= 6000) {
+    let usageStep1 = ((userWaterUse / 1000) * 15.73)
+    console.log(`Your total amount due is R ${usageStep1}`)
+}
+
+// Condition for over 6kl and under 10.5kl water usage
+else if (6000 < userWaterUse <= 10500) {
+    let usageStep2 = ((userWaterUse / 1000) * 15.73) +
+    (((userWaterUse - 6000) / 1000) * 22.38)
+    console.log(`Your total amount due is R ${usageStep2}`)
+}
+
+// Condition for over 10.5kl and under 35kl water usage
+else if (10500 < userWaterUse <= 35000) {
+    let usageStep3 = ((userWaterUse / 1000) * 15.73) +
+    (((userWaterUse - 6000) / 1000) * 22.38) +
+    (((userWaterUse - 10500) / 1000) * 31.77)
+    console.log(`Your total amount due is R ${usageStep3}`)
+}
+
+// Condition for over 35kl water usage
+else {
+    let usageStep4 = ((userWaterUse / 1000) * 15.73) +
+    (((userWaterUse - 6000) / 1000) * 22.38) +
+    (((userWaterUse - 10500) / 1000) * 31.77) +
+    (((userWaterUse - 35000) / 1000) * 31.77)
+    console.log(`Your total amount due is R ${usageStep4}`)
+}
